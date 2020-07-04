c6478eee086a0a0f5e9e5af5d7523264b0fdc19c
YooTanya <yootanya@gmail.com>
2020-07-05 01:11:48 +0700

    reset preset CSS in global style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { store } from './store';
 import App from './App';
+import reset from 'styled-reset';
 
-render(<Provider store={store}><App /></Provider>,
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    font-family: 'Notable', sans-serif;
+  }
+`
+render(<Provider store={store}> <GlobalStyle /><App /></Provider>,
   document.getElementById('root'),
 );
