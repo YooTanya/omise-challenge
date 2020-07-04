5683f65f4d19938b449c7127763c6b9e9e91124a
YooTanya <yootanya@gmail.com>
2020-07-05 01:13:18 +0700

    Add content to App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getCharities, getPayments } from './store/actions';
 import { getMessageState, getDonationState } from './store/selectors';
+import { Landing } from './components/pages/Landing';
+import Message from './components/shared/Message';
+import React, { useEffect } from 'react';
+import Theme from './styles/Theme';
+import Typography from './components/shared/Typography';
+
+const root = {
+  margin: '5rem',
+}
+
 
 const App = () => {
   const dispatch = useDispatch();
   const message = useSelector(getMessageState);
-  const donation = useSelector(getDonationState);
+  // const donation = useSelector(getDonationState);
 
   useEffect(() => {
     dispatch(getCharities());
     dispatch(getPayments());
   },[dispatch]);
 
-  return <div>TEST</div>;
+  return (
+    <Theme>
+      <div style={root}>
+        <Typography alignText="center" variant="title">Omise Tamboon React</Typography>
+        {/* <Typography alignText="center" variant="subtitle">All donations: {donation}</Typography> */}
+        {message?.success && <Message success>{message.success}</Message>}
+        {message?.error && <Message failed>{message.error}</Message>}
+        <Landing />
+      </div>
+    </Theme>
+  );
 };
 
 export default App;
