a4190bf59f030993f61c1689def1b3b715dc2b38
YooTanya <yootanya@gmail.com>
2020-07-05 11:17:27 +0700

    moved GlobalStyle to styles folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components'
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { store } from './store';
 import App from './App';
-import reset from 'styled-reset';
+import GlobalStyle from './styles/GlobalStyle'
 
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  body {
-    font-family: 'Notable', sans-serif;
-  }
-`
 render(<Provider store={store}> <GlobalStyle /><App /></Provider>,
   document.getElementById('root'),
 );
